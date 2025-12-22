18c720895586b52220ed6a2b0fd3ddabc6d7c5ab
forfeo2005 <forfeo2005@gmail.com>
2025-12-22 01:58:19 -0500

    fix: move trpc.ts to utils folder to match App.tsx import

--- forfeo-booking-demo/client/src/trpc.ts
+++
@@ -1,7 +0,0 @@
-// Fichier: client/src/utils/trpc.ts
-import { createTRPCReact } from '@trpc/react-query';
-// Assure-toi que ce chemin remonte bien jusqu'à "server/routers.ts"
-// Si ton dossier client est au même niveau que server, c'est souvent ../../../server/routers
-import type { AppRouter } from '../../../server/routers';
-
-export const trpc = createTRPCReact<AppRouter>();
